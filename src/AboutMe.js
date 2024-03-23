23f36694d4f0f881fcb10b311960037e4e7f9a6d
Rhys Smoker <r.smoker@gmail.com>
2024-03-23 12:33:49 -0700

    updated the about me

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -11,7 +11,11 @@ export default function AboutMe() {
 			<p className="about-me-text">
 				My background is in the natural sciences and math and science education.
 				I am a creative and tenacious problem solver. I'm also able to switch
-				frequently between being detail oriented and being a systems thinker.
+				frequently between being detail oriented and being a systems thinker. I
+				specialize in using the{" "}
+				<a href="https://www.mongodb.com/mern-stack">MERN stack</a>, a
+				JavaScript-based framework for building completely full stack web
+				applications from the database to the browser.
 			</p>
 		</div>
 	);
